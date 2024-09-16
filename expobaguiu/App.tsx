378378bfc16994f4cea6e8b947e7196355b2d1df
INeedMorePowerD <160240056+INeedMorePowerD@users.noreply.github.com>
2024-09-16 00:37:30 -0300

    seloco bixo atualização grande

--- expobaguiu/App.tsx
+++ expobaguiu/App.tsx
@@ -1,15 +1,29 @@
 import react from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Linking } from 'react-native';
+import { Image } from 'expo-image';
+
 
 export default function App() {
+  const handlePress = () => {
+    Linking.openURL('https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjo-rHUwcaIAxWyNTUKHXZVNKAQwqsBegQIDBAG&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DIDsrHXwfPmY&usg=AOvVaw3VBoCDDNYFIhf9SvSgla_h&opi=89978449');
+  };
   return (
     <View style={styles.container}>
       <View style ={styles.box1}>
        <Text>KeepCalm</Text>
       </View>
       <View style ={styles.box2}>
-       <Text>KeepCalmParte2</Text>
+       <Text>Era para ter uma coisa aqui mas eu esqueci de salvar, oops</Text>
       </View>
+       <Image
+         style={styles.image}
+         source={{ uri: 'https://steamuserimages-a.akamaihd.net/ugc/2480999067057807841/582D44A38AED745568D0C8E77182AE1698EE7853/?imw=637&imh=358&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true' }}
+       />
+       <TouchableOpacity onPress={handlePress}>
+       <View style={styles.box3}>
+        <Text>GET SNUCK UPON</Text>
+       </View>
+       </TouchableOpacity>
     </View>
   );
 }
@@ -17,7 +31,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightpink',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -28,6 +42,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     marginBottom: 20,
+    borderRadius: 30,
+  },
+  image: {
+    width: 400,
+    height: 200,
+    resizeMode: 'contain',
+    borderRadius: 20
   },
   box2: {
     width: 600,
@@ -35,6 +56,19 @@ const styles = StyleSheet.create({
     backgroundColor: 'lightblue',
     justifyContent: 'center',
     alignItems: 'center',
-    marginBottom: 20,
+    marginBottom: 10,
+    borderRadius: 40,
+  },
+  box3: {
+    width: 300,
+    height: 100,
+    backgroundColor: 'yellow',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 5,
+    borderRadius: 40,
+    marginTop: 5,
+    textDecorationLine: 'underline',
+    fontSize: 16,
   },
 }); 
